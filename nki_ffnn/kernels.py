8457e3deb732c5f40201112ed32b0a43d3fee7cf
rileyreichel <rileyreichel@gmail.com>
2026-04-30 07:00:02 +0000

    he

--- nki_ffnn/kernels.py
+++ nki_ffnn/kernels.py
@@ -22,7 +22,13 @@ def nki_transpose(in_tensor):
 
     out_tensor = nl.ndarray((o_rows, o_cols), dtype=in_tensor.dtype, buffer=nl.hbm)
 
-    # YOUR CODE HERE
+    TILE_SIZE = nl.tile_size.pmax
+    for i_row in nl.affine_range(i_rows // TILE_SIZE):
+        for i_col in nl.affine_range(i_cols // TILE_SIZE):
+            tile = nl.load_transpose2d(in_tensor[i_row * TILE_SIZE : (i_row + 1) * TILE_SIZE, i_col * TILE_SIZE : (i_col + 1) * TILE_SIZE])
+
+            nl.store(out_tensor[i_col * TILE_SIZE : (i_col + 1) * TILE_SIZE, i_row * TILE_SIZE : (i_row + 1) * TILE_SIZE], value=tile)
+
 
     return out_tensor
 
@@ -46,6 +52,25 @@ def nki_bias_add_act(A, b, act='relu'):
     result = nl.ndarray((BATCH_SIZE, HIDDEN_SIZE), dtype=A.dtype, buffer=nl.hbm)
 
     # YOUR CODE HERE
+    TILE_SIZE = nl.tile_size.pmax
+
+    for i_batch in nl.affine_range(BATCH_SIZE // TILE_SIZE):
+        for i_hidden in nl.affine_range(HIDDEN_SIZE // TILE_SIZE):
+            a_tile = nl.load(A[i_batch * TILE_SIZE : (i_batch + 1) * TILE_SIZE, i_hidden * TILE_SIZE : (i_hidden + 1) * TILE_SIZE])
+            b_tile = nl.load(b[0, i_hidden * TILE_SIZE : (i_hidden + 1) * TILE_SIZE])
+
+            res = nl.add(a_tile, b_tile)
+
+            if act == 'relu':
+                res = nl.relu(res)
+            elif act == 'softmax':
+                max_val = nl.max(res, axis=1)
+                res = nl.subtract(res, max_val)
+                res = nl.exp(res)
+                sum_val = nl.sum(res, axis=1)
+                res = nl.divide(res, sum_val)
+
+            nl.store(result[i_batch * TILE_SIZE : (i_batch + 1) * TILE_SIZE, i_hidden * TILE_SIZE : (i_hidden + 1) * TILE_SIZE], value=res)
 
     return result
 
@@ -86,9 +111,15 @@ def nki_forward(
 
   # Layer 1
   # YOUR CODE HERE  
+  W1T = nki_transpose(W1)
+  H = nki_matmul(W1T, X)
+  H = nki_bias_add_act(H, b1, act='relu')
 
   # Layer 2 (output)
   # YOUR CODE HERE
+  W2T = nki_transpose(W2)
+  probs = nki_matmul(W2T, H)
+  probs = nki_bias_add_act(probs, b2, act='softmax')
 
   return probs
 
@@ -120,10 +151,22 @@ def nki_predict(
   Returns:
       predictions: a 1D tensor of shape [BATCH_SIZE] with the predicted class for each input
   """
-  probs = # YOUR CODE HERE
+
+    # YOUR CODE HERE
+  probs = nki_forward(X, W1, b1, W2, b2, matmul_kernel=matmul_kernel)
   BATCH_SIZE, OUTPUT_SIZE = probs.shape
   predictions = nl.ndarray((BATCH_SIZE,), dtype=np.int32, buffer=nl.hbm)
 
-  # YOUR CODE HERE
+  TILE_SIZE = nl.tile_size.pmax
 
+  for i_batch in nl.affine_range(BATCH_SIZE // TILE_SIZE):
+        probs_tile = nl.load(probs[i_batch * TILE_SIZE : (i_batch + 1) * TILE_SIZE, 0 : OUTPUT_SIZE])
+
+        max_vals = nisa.tensor_reduce(np.max, probs_tile, axis=1, keepdims=True)
+
+        argmax_result = nisa.argmax(probs_tile, axis=1)
+
+        argmax_indices = argmax_result[:, 0]
+
+        nl.store(predictions[i_batch * TILE_SIZE : (i_batch + 1) * TILE_SIZE], value=argmax_indices)
   return predictions
