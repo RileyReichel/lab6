a828c807986a52270d78e0a21512575b87e7c200
rileyreichel <rileyreichel@gmail.com>
2026-05-02 04:41:59 +0000

    optimized

--- nki_conv2d/conv2d.py
+++ nki_conv2d/conv2d.py
@@ -26,7 +26,6 @@ Note:
     - output_channels % 128 == 0
     - output_width * output_height % 512 == 0
 """
-
 @nki.jit
 def conv2d_nki(X, W, bias):
     batch_size, in_channels, input_height, input_width = X.shape
@@ -54,23 +53,25 @@ def conv2d_nki(X, W, bias):
  
     for img in nl.affine_range(batch_size):
         for c_out_tile_idx in nl.affine_range(n_tiles_c_out):
+            bias_tile = nl.load(bias[c_out_tile_idx * c_out_tile : (c_out_tile_idx + 1) * c_out_tile])
+            bias_tile_2d = bias_tile.reshape((c_out_tile, 1))
+ 
             for out_row in nl.affine_range(out_height):
                 row_out = nl.zeros((c_out_tile, out_width), dtype=nl.float32, buffer=nl.psum)
  
                 for c_in_tile_idx in nl.affine_range(n_tiles_c_in):
+                    w_all = nl.ndarray((c_out_tile, c_in_tile, filter_height, filter_width), dtype=W.dtype, buffer=nl.sbuf)
+                    nisa.dma_copy(dst=w_all, src=W[c_out_tile_idx * c_out_tile : (c_out_tile_idx + 1) * c_out_tile, c_in_tile_idx * c_in_tile : (c_in_tile_idx + 1) * c_in_tile, 0:filter_height, 0:filter_width])
+ 
                     for i in nl.affine_range(filter_height):
                         for j in nl.affine_range(filter_width):
-                            w_tile = nl.ndarray((c_out_tile, c_in_tile), dtype=W.dtype, buffer=nl.sbuf)
-                            nisa.dma_copy(dst=w_tile, src=W[c_out_tile_idx * c_out_tile : (c_out_tile_idx + 1) * c_out_tile, c_in_tile_idx * c_in_tile : (c_in_tile_idx + 1) * c_in_tile, i, j])
-                            w_tile_t = nisa.nc_transpose(w_tile)
+                            w_tile_t = nisa.nc_transpose(w_all[:, :, i, j])
                             x_tile = nl.load(X[img, c_in_tile_idx * c_in_tile : (c_in_tile_idx + 1) * c_in_tile, i + out_row, j : j + out_width])
                             row_out += nisa.nc_matmul(w_tile_t, x_tile)
  
                 row_out_sbuf = nl.copy(row_out, dtype=X.dtype)
-                bias_tile = nl.load(bias[c_out_tile_idx * c_out_tile : (c_out_tile_idx + 1) * c_out_tile])
-                bias_tile_2d = bias_tile.reshape((c_out_tile, 1))
                 row_out_sbuf = nl.add(row_out_sbuf, bias_tile_2d)
- 
                 nl.store(X_out[img, c_out_tile_idx * c_out_tile : (c_out_tile_idx + 1) * c_out_tile, out_row, 0 : out_width], value=row_out_sbuf)
  
     return X_out
+ 
